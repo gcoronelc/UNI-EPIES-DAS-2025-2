b2b7e390f269ba09c6ca7cf9ce83b5262274cc5c
Gustavo Coronel <gcoronelc@gmail.com>
2025-10-23 16:52:31 -0500

    Semana 9

---
+++ Semana09/SQLQuery1.sql
@@ -0,0 +1,38 @@
+-- Entendiendo las publicaciones
+-- que se comercializan.
+
+declare @tipo char(3);
+set @tipo = 'REV';
+select * from TIPO where idtipo=@tipo;
+select * from PUBLICACION where idtipo=@tipo;
+go
+
+-- Entendiendo los empleados
+-- y los usuarios
+
+select * from EMPLEADO;
+select * from USUARIO;
+go
+
+-- Entendiendo las ventas
+-- y las promociones
+
+select * from VENTA;
+select * from  PROMOCION;
+go
+
+
+-- Tabla de parametros del sistema
+
+select * from CONTROL;
+go
+
+
+
+
+
+
+
+
+
+
